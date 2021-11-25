565c23be3691ff1040040d3b034dc44fa3723def
Esteban+1-at-667301952356 <miunmn>
2021-11-25 00:00:06 -0500

    update both p

--- p2.py
+++ p2.py
@@ -1,11 +1,16 @@
 from rtree import index
 import face_recognition
 from pathlib import Path
+import os 
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
 
 def get_encodings(Rtree):
     i = 0
     final_encodings=[]
-    path = "lfw-a\lfw\\"
+    path = "lfw-a/lfw/"
     basepath = Path(path)
     for entry in basepath.iterdir():
         if entry.is_dir():
@@ -20,19 +25,48 @@ def get_encodings(Rtree):
                         listAux = list(faceEncoding)
                         for cord in faceEncoding:
                             listAux.append(cord)
-                        Rtree.insert(i, listAux, ( path +  str(name) + '\\' + str(item.name) ))
+                        Rtree.insert(i, listAux, ( path +  str(name) + '/' + str(item.name) ))
                         final_encodings.append(listAux)
                         i= i+ 1
     Rtree.close()
     return final_encodings  
 
+def generate_distances():
+    distances = []
+    people = []
+    encodings = []
+
+    for root, folder, files in os.walk('lfw-a/'):
+        if root == '../input/alldata/Project/data/lfw/':
+            continue
+        for item in files:
+            print(item)
+            people.append(os.path.join(root, item))
+
+    N = 500
+    
+    for i in range (N):
+        print("Iteration", i)
+        random_person_1 = random.sample(people, 1)[0]
+        random_person_2 = random.sample(people, 1)[0]
+
+        image_person_1 = face_recognition.load_image_file(random_person_1)
+        image_person_2 = face_recognition.load_image_file(random_person_2)
+
+        face_encoding_1 = face_recognition.face_encodings(image_person_1)
+        face_encoding_2 = face_recognition.face_encodings(image_person_2)
+        
+        face_encoding_1 = np.array(face_encoding_1)
+        face_encoding_2 = np.array(face_encoding_2)
+        
+        try:
+            distances.append(np.linalg.norm(face_encoding_1 - face_encoding_2))
+        except:
+            print("Failed images", random_person_1, random_person_2)
+
+    return distances
+
 if __name__ == "__main__":
-    p = index.Property()
-    p.dimension = 128 #D
-    p.buffering_capacity = 10 #M
-    Rtree = index.Index('RtreeLab', properties=p)
-    encodings = get_encodings(Rtree)
-    for i in encodings:
-        print(encodings)
-
-
+    distances = generate_distances()
+    plt.hist(distances, 10)
+    plt.show()
--- p3.py
+++ p3.py
@@ -3,9 +3,10 @@ from rtree import index
 from pathlib import Path
 import numpy as np
 import heapq
-
+import os
+import random
 def knn_search_sequential_pq(k, Q, n):  
-    path = "lfw-a\lfw\\"
+    path = "lfw-a/lfw/"
     basepath = Path(path)
     aux = 0  
     route = []
@@ -49,7 +50,7 @@ def knn_search_rtree(k, Q):
         coordinatesListQuery.append(i)
 
     i = 0
-    path = "lfw-a\lfw\\"
+    path = "lfw-a/lfw/"
     basepath = Path(path)
     for entry in basepath.iterdir():
         if entry.is_dir():
@@ -64,7 +65,7 @@ def knn_search_rtree(k, Q):
                         listAux = list(faceEncoding)
                         for cord in faceEncoding:
                             listAux.append(cord)
-                        Rtree.insert(i, listAux, ( path +  str(name) + '\\' + str(item.name) ))
+                        Rtree.insert(i, listAux, ( path +  str(name) + '/' + str(item.name) ))
                         i= i+ 1
     return list(Rtree.nearest(coordinates=coordinatesListQuery ,num_results=k, objects='raw'))
 
@@ -78,16 +79,37 @@ def range_search_rtree(query, radio):
     p.dimension = 128
     p.buffering_capacity = 10
     Rtree = index.Index('RtreeRangeSearch', properties=p)
+    i = 0
+    path = "lfw-a/lfw/"
+    basepath = Path(path)
+    for entry in basepath.iterdir():
+        if entry.is_dir():
+            name = entry.name
+            files = Path(path + str(name))
+            files_in_basepath = files.iterdir()
+            for item in files_in_basepath:
+                if item.is_file():
+                    image = face_recognition.load_image_file(item)
+                    facesEncoding = face_recognition.face_encodings(image)
+                    for faceEncoding in facesEncoding:
+                        listAux = list(faceEncoding)
+                        for cord in faceEncoding:
+                            listAux.append(cord)
+                        Rtree.insert(i, listAux, ( path +  str(name) + '/' + str(item.name) ))
+                        i= i+ 1
+
+
     return [
         n.object for n in Rtree.intersection(bounding_box(query, radio), objects=True)
         if n is not None and isinstance(n.object, str)
     ]
 
-
 if __name__ == "__main__":
-    print("knn_search_sequential_pq: ")
-    print(knn_search_sequential_pq(3, face_recognition.face_encodings(face_recognition.load_image_file('lfw-a\lfw\Aaron_Pena\Aaron_Pena_0001.jpg'))[0], 50))
-    print("knn_search_rtree: ")
-    print(knn_search_rtree(3, face_recognition.face_encodings(face_recognition.load_image_file('lfw-a\lfw\Aaron_Pena\Aaron_Pena_0001.jpg'))[0]))
-    print("range_search_rtree: ")
-    print(range_search_rtree(face_recognition.face_encodings(face_recognition.load_image_file('lfw-a/lfw/Aaron_Pena/Aaron_Pena_0001.jpg'))[0], 3.0 ))
+    # print("knn_search_sequential_pq: ")
+    # print(knn_search_sequential_pq(3, face_recognition.face_encodings(face_recognition.load_image_file('lfw-a/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg'))[0], 50))
+    # print("knn_search_rtree: ")
+    # print(knn_search_rtree(3, face_recognition.face_encodings(face_recognition.load_image_file('lfw-a/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg'))[0]))
+    # print("range_search_rtree: ")
+    # print(range_search_rtree(face_recognition.face_encodings(face_recognition.load_image_file('lfw-a/lfw/Aaron_Eckhart/Aaron_Eckhart_0001.jpg'))[0], 3.0 ))
+    distances = generate_distances()
+
